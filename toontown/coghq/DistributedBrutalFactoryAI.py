349ad2bc4a47550d7c047d8fd6c58b31e2d597ad
Chan <chandler14362@gmail.com>
2015-03-10 13:25:40 -0430

    Cleaned up and optimized DBrutalFactoryAI

--- toontown/coghq/DistributedBrutalFactoryAI.py
+++ toontown/coghq/DistributedBrutalFactoryAI.py
@@ -1,24 +1,22 @@
-from direct.directnotify import DirectNotifyGlobal
-import DistributedFactoryAI
+from toontown.coghq.DistributedFactoryAI import DistributedFactoryAI
 from toontown.toon import NPCToons
+
 import random
 
-class DistributedBrutalFactoryAI(DistributedFactoryAI.DistributedFactoryAI):
-    notify = DirectNotifyGlobal.directNotify.newCategory('DistributedBrutalFactoryAI')
 
-    def __init__(self, air, factoryId, zoneId, entranceId, avIds):
-        DistributedFactoryAI.DistributedFactoryAI.__init__(self, air, factoryId, zoneId, entranceId, avIds)
+class DistributedBrutalFactoryAI(DistributedFactoryAI):
+    notify = directNotify.newCategory('DistributedBrutalFactoryAI')
+
+    POSSIBLE_SOS = NPCToons.npcFriendsMinMaxStars(3, 3)
 
     def setVictors(self, victorIds):
-        DistributedFactoryAI.DistributedFactoryAI.setVictors(self, victorIds)
+        DistributedFactoryAI.setVictors(self, victorIds)
 
-        activeVictors = []
-        for victorId in victorIds:
-            toon = self.air.doId2do.get(victorId)
-            if toon is not None:
-                activeVictors.append(toon)
-        npcId = random.choice(NPCToons.npcFriendsMinMaxStars(3, 3))
-        for toon in activeVictors:
-            toon.attemptAddNPCFriend(npcId)
-            toon.d_setSystemMessage(0, 'You got a %s SOS card.' % (NPCToons.getNPCName(npcId)))
-
+        activeVictors = [self.air.doId2do.get(victorId) for victorId in victorIds if self.air.doId2do.get(victorId) is not None]
+        
+        npcId = random.choice(self.POSSIBLE_SOS)
+        npcName = NPCToons.getNPCName(npcId)
+        
+        for victor in activeVictors:
+            victor.attemptAddNPCFriend(npcId)
+            victor.d_setSystemMessage(0, 'You got a %s SOS card.' % npcName)
